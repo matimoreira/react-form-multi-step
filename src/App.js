47432b19b8a4d891992ab274e4bba202cc02bd0e
matimoreira <58670479+matimoreira@users.noreply.github.com>
2020-03-23 07:43:47 -0300

    Agregue uso de hocks en marathon.jsx

--- src/App.js
+++ src/App.js
@@ -1,137 +1,58 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+import Marathones from './Marathones';
 
-import Header from './Components/Header';
-import FormMultiStep from './Components/FormMultiStep';
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
 
-class App extends React.Component {
-	title = 'Maratones';
-	state = 
-	{
-		datosMarathones: [{
-			IdMarathon: 1, 
-			title: '', 
-			RaceStartDate: '2020-05-03 00:00:00.000', 
-			RegistrationDeadline: '2020-05-02 00:00:00.000',
-			RegistrationStartDate: '2020-04-02 00:00:00.000',
-			InitialLatitude: '-27.36543787922855',
-			InitialLongitude: '-55.916355238035294',
-			FinalLatitude: '-27.377938174767543',
-			FinalLongitude: '-55.916355238035294',
-			CompetitorsLimit: 100,
-			category: 1,
-			detail: 'Carrera en favor de los niños de Africa',
-			JsonUploaded: false
-		}],
-		datosMarathon: {
-			title: '', 
-			RaceStartDate: '', 
-			RegistrationDeadline: '',
-			RegistrationStartDate: '',
-			InitialLatitude: '',
-			InitialLongitude: '',
-			FinalLatitude: '',
-			FinalLongitude: '',
-			CompetitorsLimit: 100,
-			category: 1,
-			detail: '',
-			JsonUploaded: false
-		},
-		datosMap:{},
-		pagination: { page: 0, pageSize: 5, pageTotal: 1}
-	};
+export default function BasicExample() {
+	return (
+		<Router>
+			<div>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/marathon">Marathon</Link>
+					</li>
+					<li>
+						<Link to="/emulator">Emulador</Link>
+					</li>
+				</ul>
 
-	handleAddMarathonStepOne = (marathon) => {
-		let state = this.state;
-		state.datosMarathon.title = marathon.title;
+				<hr />
 
-		state.datosMarathon.CompetitorsLimit = parseInt(marathon.competitors_limit);
-		state.datosMarathon.category = parseInt(marathon.category);
-		state.datosMarathon.detail = marathon.detail;
-
-		this.setState(state);
-	}
-	handleAddMarathonStepTwo = (marathon) => {
-		let state = this.state;
-
-		state.datosMarathon.RaceStartDate = marathon.race_start_date;
-		state.datosMarathon.RegistrationDeadline = marathon.registration_deadline;
-		state.datosMarathon.RegistrationStartDate = marathon.registration_start_date;
-
-		this.setState(state);
-	}
-	handleAddMarathonStepThree = async (datosMap) => {
-		let state = this.state;
-
-		let LngLatIni = datosMap.route[0];
-		let LngLatFin = datosMap.route[datosMap.route.length-1];
-
-		state.datosMarathon.InitialLongitude = ''+LngLatIni[0];
-		state.datosMarathon.InitialLatitude = ''+LngLatIni[1];
-
-		state.datosMarathon.FinalLongitude = ''+LngLatFin[0];
-		state.datosMarathon.FinalLatitude = ''+LngLatFin[1];
-
-		state.datosMap = datosMap;
-
-		this.setState(state);
-
-		let newMarathon = await this.postMarathon();
-		this.postParades(newMarathon);
-	}
-
-	settingsPost = {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	}
-
-	postMarathon = async () => {
-
-		this.settingsPost.body = this.state.datosMarathon;
-		
-		try{
-			let response = await fetch('https://localhost:44308/api/Marathons/PostMarathon', this.settingsPost);
-			return await response.json();
-		} catch(e) {
-			console.log('fallo post marathon', e);
-		}
-	}
-
-	postParades = async (newMarathon) => {
-		
-		console.log('New marathon', newMarathon.idMarathon);
-		this.settingsPost.body = newMarathon.idMarathon;
-
-		try{
-			//let response = await fetch('https://localhost:44308/api/Marathons/PostMarathon', settings);
-			//return response;
-		} catch(e) {
-			console.log('fallo post parades', e);
-		}
-	} 
-
-
-	render(){
-		return(
-			<div className="container mt-3">
-				<Header title={this.title}></Header>
-				<div className="row">
-					<div className="col-md-6">
-						<FormMultiStep 
-							addMarathonStepOne={this.handleAddMarathonStepOne} 
-							addMarathonStepTwo={this.handleAddMarathonStepTwo}
-							addMarathonStepThree={this.handleAddMarathonStepThree}></FormMultiStep>
-					</div>
-					<div className="col-md-6">
-
-					</div>					
-				</div>
+				{/*
+					A <Switch> looks through all its children <Route>
+					elements and renders the first one whose path
+					matches the current URL. Use a <Switch> any time
+					you have multiple routes, but you want only one
+					of them to render at a time
+				*/}
+				<Switch>
+					<Route exact path="/">
+						<div>Home</div>
+					</Route>
+					<Route path="/marathon">
+						<Marathones />
+					</Route>
+					<Route path="/emulator">
+						<div>Emulator</div>
+					</Route>
+				</Switch>
 			</div>
-
-		)
-	}
+		</Router>
+	);
 }
-
-export default App;
